ff9a3030cfd8eab3ed20c8d1b71f4abd6d2e5d3c
Harshita upadhyay <harshita@Harshitas-MacBook-Air.local>
2023-07-25 12:48:11 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "766c93ae7701b50bbaae3f22bcb87128",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
